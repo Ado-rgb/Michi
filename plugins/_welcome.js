cdd9a48d9b8ebaea5af8cf31e116b0b481cb0cea
Ado <minexdt@gmail.com>
2025-07-23 06:42:26 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -17,7 +17,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   }
 
   if (chat.welcome && m.messageStubType == 27) {
-    let bienvenida = `\`✦ *Bienvenid@* a\` ${groupMetadata.subject}\n➮ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✐ Ahora somos ${groupSize} Miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo!\n> ☄︎ Puedes usar #menu para ver la lista de comandos.`    
+    let bienvenida = `\`✦ Bienvenid@ a ${groupMetadata.subject}\`\n➮ @${m.messageStubParameters[0].split`@`[0]}\n${global.welcom1}\n✐ Ahora somos ${groupSize} miembros.\n•(=^●ω●^=)• Disfruta tu estadía en el grupo :)\n> ☄︎ Puedes usar #menu para ver la lista de comandos.`    
     await conn.sendMini(m.chat, txt, dev, bienvenida, img, img, redes, fkontak)
   }
   
