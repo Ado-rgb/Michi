ed9f679931ce8469db4aa9c4abbccffa9fcc4d19
Ado <minexdt@gmail.com>
2025-07-25 08:53:16 -0600

    Actualizar _validCommand.js

--- plugins/_validCommand.js
+++ plugins/_validCommand.js
@@ -10,24 +10,29 @@ export async function before(m) {
   if (!command) return
   if (command === 'bot') return
 
-  const validCommand = (command, plugins) => {
+  const validCommand = (cmd, plugins) => {
+    if (!plugins || typeof plugins !== 'object') return false
     for (let plugin of Object.values(plugins)) {
+      if (!plugin.command) continue
       let cmds = Array.isArray(plugin.command) ? plugin.command : [plugin.command]
-      if (cmds && cmds.includes(command)) return true
+      if (cmds.map(c => c.toLowerCase()).includes(cmd)) return true
     }
     return false
   }
 
   if (validCommand(command, global.plugins)) {
-    let chat = global.db.data.chats[m.chat]
-    let user = global.db.data.users[m.sender]
+    let chat = global.db.data.chats[m.chat] || {}
+    let user = global.db.data.users[m.sender] || {}
+
     if (chat.isBanned) {
       const avisoDesactivado = `《✦》El bot *${botname}* está desactivado en este grupo.\n\n> ✦ Un *administrador* puede activarlo con:\n» *${usedPrefix}bot on*`
       await m.reply(avisoDesactivado)
       return
     }
+
     if (!user.commands) user.commands = 0
     user.commands += 1
+    global.db.data.users[m.sender] = user
   } else {
     const comando = m.text.trim().split(' ')[0]
     await m.reply(
