d3842121544d138076e3e507b1c3eca8ba393f81
Ado <minexdt@gmail.com>
2025-07-24 20:39:33 -0600

    Actualizar settings.js

--- settings.js
+++ settings.js
@@ -61,7 +61,7 @@ global.etiqueta = 'Yosue | Ado ⚡'
 global.moneda = '¥enes'
 global.welcom1 = '❍ Edita Con El Comando setwelcome'
 global.welcom2 = '❍ Edita Con El Comando setbye'
-global.banner = 'https://files.catbox.moe/jn4vtw.jpg'
+global.banner = 'https://qu.ax/UQmsD.jpg'
 global.avatar = 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1742678797993.jpeg'
 
 
