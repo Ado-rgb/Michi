9032b24e2c0e554bcd88acfb9979afdf5cdb7bb5
Ado <minexdt@gmail.com>
2025-07-25 06:58:18 -0600

    Actualizar _autodetect.js

--- plugins/_autodetect.js
+++ plugins/_autodetect.js
@@ -29,45 +29,55 @@ handler.before = async function (m, { conn, participants, groupMetadata }) {
   let usuario = `@${m.sender.split`@`[0]}`
   let pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || 'https://files.catbox.moe/xr2m6u.jpg'
 
-  
-  let nombre = `❀ *Cambio de Nombre*\n➪ Usuario › ${usuario}\n> Nuevo nombre › *${m.messageStubParameters[0]}*`
-  let foto = `❀ *Cambio de Imagen*\n➪ Realizado por › ${usuario}`
-  let edit = `❀ *Configuración del grupo actualizada*\n➪ Ahora › ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} pueden cambiar ajustes.`
-  let newlink = `❀ *Enlace del grupo restablecido*\n➪ Acción › ${usuario}`
-  let status = `❀ *Estado del grupo*\n➪ ${m.messageStubParameters[0] == 'on' ? '*Cerrado*' : '*Abierto*'} por ${usuario}\n> Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensajes.`
-  let admingp = `❀ *Nuevo Administrador*\n➪ ${m.messageStubParameters[0]} ahora es admin\n> Acción de › ${usuario}`
-  let noadmingp = `❀ *Admin Removido*\n➪ ${m.messageStubParameters[0]} dejó de ser admin\n> Acción de › ${usuario}`
+  let nombre = `❀ ${usuario} Ha cambiado el nombre del grupo.\n\n> ✦ Ahora el grupo se llama:\n> *${m.messageStubParameters[0]}*.`
+  let foto = `❀ Se ha cambiado la imagen del grupo.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
+  let edit = `❀ ${usuario} Ha permitido que ${m.messageStubParameters[0] == 'on' ? 'solo admins' : 'todos'} puedan configurar el grupo.`
+  let newlink = `❀ El enlace del grupo ha sido restablecido.\n\n> ✦ Acción hecha por:\n> » ${usuario}`
+  let status = `❀ El grupo ha sido ${m.messageStubParameters[0] == 'on' ? '*cerrado*' : '*abierto*'} por ${usuario}\n\n> ✦ Ahora ${m.messageStubParameters[0] == 'on' ? '*solo admins*' : '*todos*'} pueden enviar mensaje.`
+
+  let admingp = `❀ *Nuevo Administrador*\n➪ @${m.messageStubParameters[0].split('@')[0]} ahora es admin\n> Acción de › ${usuario}`
+  let noadmingp = `❀ *Admin Removido*\n➪ @${m.messageStubParameters[0].split('@')[0]} dejó de ser admin\n> Acción de › ${usuario}`
 
   if (chat.detect && m.messageStubType == 2) {
     const uniqid = (m.isGroup ? m.chat : m.sender)
     const sessionPath = './Sessions/'
+
     for (const file of await fs.readdir(sessionPath)) {
       if (file.includes(uniqid)) {
         await fs.unlink(path.join(sessionPath, file))
-        console.log(`${chalk.yellow.bold('[Archivo Eliminado]')} ${chalk.greenBright(`'${file}'`)}\n` +
-          `${chalk.blue('(Session PreKey)')} ${chalk.redBright('que causaba "undefined" en el chat')}`)
+        console.log(`${chalk.yellow.bold('[ Archivo Eliminado ]')} ${chalk.greenBright(`'${file}'`)}\n` +
+          `${chalk.blue('(Session PreKey)')} ${chalk.redBright('que provoca el "undefined" en el chat')}`
+        )
       }
     }
+
   } else if (chat.detect && m.messageStubType == 21) {
     await this.sendMessage(m.chat, { text: nombre, mentions: [m.sender] }, { quoted: fkontak })  
+
   } else if (chat.detect && m.messageStubType == 22) {
     await this.sendMessage(m.chat, { image: { url: pp }, caption: foto, mentions: [m.sender] }, { quoted: fkontak })
+
   } else if (chat.detect && m.messageStubType == 23) {
     await this.sendMessage(m.chat, { text: newlink, mentions: [m.sender] }, { quoted: fkontak })
+
   } else if (chat.detect && m.messageStubType == 25) {
     await this.sendMessage(m.chat, { text: edit, mentions: [m.sender] }, { quoted: fkontak })  
+
   } else if (chat.detect && m.messageStubType == 26) {
     await this.sendMessage(m.chat, { text: status, mentions: [m.sender] }, { quoted: fkontak })  
+
   } else if (chat.detect && m.messageStubType == 29) {
     await this.sendMessage(m.chat, { text: admingp, mentions: [m.sender, m.messageStubParameters[0]] }, { quoted: fkontak })
+
   } else if (chat.detect && m.messageStubType == 30) {
     await this.sendMessage(m.chat, { text: noadmingp, mentions: [m.sender, m.messageStubParameters[0]] }, { quoted: fkontak })
+
   } else {
     if (m.messageStubType == 2) return
     console.log({
       messageStubType: m.messageStubType,
       messageStubParameters: m.messageStubParameters,
-      type: WAMessageStubType[m.messageStubType]
+      type: WAMessageStubType[m.messageStubType], 
     })
   }
 }
