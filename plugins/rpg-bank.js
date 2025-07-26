e7ab40cbc38fae658364076db6bce918dadaf682
Ado <minexdt@gmail.com>
2025-07-25 21:42:02 -0600

    Actualizar rpg-bank.js

--- plugins/rpg-bank.js
+++ plugins/rpg-bank.js
@@ -1,29 +1,24 @@
 import db from '../lib/database.js'
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
-    if (who == conn.user.jid) return m.react('✖️')
-    if (!(who in global.db.data.users)) return m.reply(`${emoji} El usuario no se encuentra en mi base de datos*`)
-  
-    let user = global.db.data.users[who]
-    let total = (user.coin || 0) + (user.bank || 0);
+  let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+  if (who == conn.user.jid) return m.react('✖️')
+  if (!(who in global.db.data.users)) return m.reply(`❌ Ese usuario ni figura en la base de datos w`)
 
-    const texto = `ᥫ᭡ Informacion -  Economia ❀
- 
-ᰔᩚ Usuario » *${conn.getName(who)}*   
-⛀ Dinero » *${user.coin} ${moneda}*
-⚿ Banco » *${user.bank} ${moneda}*
-⛁ Total » *${total} ${moneda}*
+  let user = global.db.data.users[who]
+  let total = (user.coin || 0) + (user.bank || 0)
 
-> *Para proteger tu dinero, ¡depósitalo en el banco usando #deposit!*`;
+  const texto = `❀ *Información de Balance*\n
+➪ *Usuario ›* ${conn.getName(who)}\n> ⛁ Dinero › *${user.coin} ${moneda}*\n> ⚿ Banco › *${user.bank} ${moneda}*\n> ☁︎ Total › *${total} ${moneda}*\n
+> *Tip ›* Guarda tu feria en el banco usando *${usedPrefix}deposit* antes que te la vuelen alv 💸`
 
-    await conn.reply(m.chat, texto, m)
+  await conn.reply(m.chat, texto, m)
 }
 
 handler.help = ['bal']
-handler.tags = ['rpg']
-handler.command = ['bal', 'balance', 'bank'] 
-handler.register = true 
-handler.group = true 
+handler.tags = ['economy']
+handler.command = ['bal', 'balance', 'bank']
+handler.register = true
+handler.group = true
 
 export default handler
